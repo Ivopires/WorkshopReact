b7e8b0fb03d12668efc9c58709cbf3d50d269bd9
Ivopires <ivopires.93@gmail.com>
2017-11-20 23:58:45 +0000

    On the todoList components/App.js

--- workshop/src/index.js
+++ workshop/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux'
+import todoApp from './reducers/todoApp'
+
+let store = createStore(todoApp, window.STATE_FROM_SERVER)
 
 ReactDOM.render(<App />, document.getElementById('root'));
