7329b0d409c4bdab4f17f319bec7c6036058514e
Ivopires <ivopires.93@gmail.com>
2017-11-21 00:01:56 +0000

    The App.js done

--- workshop/src/components/App.js
+++ workshop/src/components/App.js
@@ -3,4 +3,12 @@ import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
 
+const App = () => (
+    <div>
+        <AddTodo />
+        <VisibleTodoList/>
+        <Footer/>
+    </div>
+)
 
+export default App
